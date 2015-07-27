3ed6ca2d4429f53f377379e485985124e1d86454
Terence Ge <Terencegehui@yahoo.com>
2015-07-28 02:19:40 +0800

    add animations

---
+++ public/javascripts/wave.js
@@ -0,0 +1,33 @@
+
+var yoff = 0.0; 
+
+function setup() {
+   createCanvas(1600, 400);
+}
+
+function drawWave(color, option) {
+  fill(color);
+  noStroke();
+  beginShape(); 
+  
+  var xoff = 0; 
+  for (var x = -100; x <= width + 100; x += option) {
+    var y = map(noise(xoff, yoff), 0, 1, 200, 300);
+    vertex(x, y); 
+    xoff += 0.05;
+  }
+  yoff += 0.004;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+}
+
+function draw() {
+  background(0, 0, 0);
+  blendMode(SCREEN);
+  drawWave(color(60, 208, 123), 22);
+  drawWave(color(223, 87, 87), 11);
+  drawWave(color(120, 36, 147), 30);
+  drawWave(color(43, 89, 244), 29);
+  drawWave(color(60, 208, 123), 8);
+}
